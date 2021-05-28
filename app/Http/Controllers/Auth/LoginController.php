c66b4063f4d4a84da50c0df4acb636f4a94db2d2
ricointhehouseyo <rico.smartlolo@gmail.com>
2021-05-28 09:21:33 +0700

    laravel sanctum

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        //
+    }
+}
